966a5a4f02e13111a5f54021bd08f6e68641ae01
yassinerouis <56519992+yassinerouis@users.noreply.github.com>
2020-11-06 10:32:47 +0100

    configuration du projet

---
+++ src/main/java/org/sid/sigital/SigitalAuthentificationApplication.java
@@ -0,0 +1,13 @@
+package org.sid.sigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigitalAuthentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigitalAuthentificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sid/sigital/SigitalAuthentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.sigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigitalAuthentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
