a0779f29680789eb927fb299633ce49afa73dcb0
yassinerouis <56519992+yassinerouis@users.noreply.github.com>
2020-11-06 10:56:20 +0100

    creation de la couche dao

---
+++ src/main/java/org/sid/sigital/dao/RoleRepository.java
@@ -0,0 +1,9 @@
+package org.sid.sigital.dao;
+
+
+import org.sid.sigital.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
